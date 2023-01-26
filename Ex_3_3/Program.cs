90d9327837310682adb2ee50c3b855e92be89bf0
pidzhak <pidzhak84@gmail.com>
2023-01-26 23:17:29 +0300

    massiv

---
+++ Ex_3_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Дан текст. В нём нужно все пробелы заменить чёрточками, маленькие буквы «к»
+// заменить большими «К», а большие «С» заменить на маленькие «с».
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replase(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+    int lenght = text.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+    if (text[i] == oldValue) result = result + $"{newValue}";
+    else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+string newText = Replase(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replase(text, 'к', 'К');
+Console.WriteLine(newText);
